54ae919a3a33e44ad9f36ebf6d05a7e2c0f973cd
moyo <git@moyo.im>
2019-07-29 11:09:55 +0800

    add logger context tests

--- tests/LoggerTest.php
+++ tests/LoggerTest.php
@@ -14,16 +14,25 @@ use function Carno\Config\conf;
 
 class LoggerTest extends TestCase
 {
+    private const DAT = [
+        'k1' => 'v1',
+        'k2' => 111,
+        'k3' => true,
+        'k4' => null,
+        'k5' => [1, 2, 3],
+        'k6' => ['kk1' => 'vv1']
+    ];
+
     public function testFunc()
     {
         conf()->set('log.addr', '???');
-        logger('test')->info('test1');
+        logger('test')->info('test1', self::DAT);
 
         conf()->set('test.log.format', 'json');
-        logger('test')->info('test2');
+        logger('test')->info('test2', self::DAT);
 
         conf()->set('log.level', 'debug');
-        logger()->debug('test3');
+        logger()->debug('test3', self::DAT);
 
         $this->assertTrue(logger() instanceof Logger);
     }
